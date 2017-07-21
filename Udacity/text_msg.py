af53db645a205005745615337cb85eec2a706076
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2017-07-21 14:57:03 +0530

    Update text_msg.py

--- Udacity/text_msg.py
+++ Udacity/text_msg.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC9fec95484d85a67cbbb67c87fbc743f3"
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 # Your Auth Token from twilio.com/console
-auth_token  = "1d88c36509908108b54f68e23229fb9b"
+auth_token  = "your_auth_token" #your token generated on your twilio account
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+918126673095", 
-    from_="+12566678909",
+    to="your registered number", 
+    from_="your's twilio number",
     body="Hello! this is Ritesh. This is a programmable sms send using Python!")
 
 print(message.sid)
